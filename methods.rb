c313b90de760e9edf5a320669eabecb9cfb332ba
moringa <oscarokumu47@gmail.com>
2022-10-18 15:27:38 +0300

    Done with lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#Test 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#Test 3
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#Test 4
+def add(num1, num2)
+    return num1 + num2
+end
+
+#Test 5
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
